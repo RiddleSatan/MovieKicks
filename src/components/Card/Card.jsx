39688d93e70cd5a523eae0f47120b40e6e60288e
RiddleSatan <mynameisompandey007@gmail.com>
2024-02-12 13:58:52 +0530

    app is ready

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,36 +1,94 @@
 import React from "react";
 import { FaDownload } from "react-icons/fa";
-import useInfo from "../Hooks/useInfoHook.js";
-
-// console.log(info)
+import useInfo from "../Hooks/useInfoHook";
+import useFetch from "../Hooks/useInfoHook";
 
 const Card = () => {
-    // const info=useInfo();
-    
+  const { data, loading, error } = useFetch("spiderman");
   return (
     <>
-      <div className="w-[12%]  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-          <img className="rounded-t-lg" src="https://m.media-amazon.com/images/M/MV5BOWZmOGQyNDctYWFjMC00NWQxLTg1YWQtMzExZWQzNjgzZDBiXkEyXkFqcGdeQXVyMTk3OTQ4ODk@._V1_.jpg" alt="" />
-        </a>
-        <div className="p-2">
-          <a href="#">
-            <h5 className="mb-2 text-2xl mx-auto font-bold tracking-tight text-gray-900 dark:text-white">
-              The Boy 2021
-            </h5>
-          </a>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
-          <a
-            href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Download
-            <FaDownload className="mx-2" />
-          </a>
-        </div>
-      </div>
+      
+        {
+          loading?(
+            <h1>loading...</h1>
+          ):error?(
+            <h1>{error}</h1>
+          ):(
+            <div className="w-[90%] flex gap-20 flex-wrap mx-auto">
+                 {data.map((info,id)=>(
+                  <div key={id} className="w-[18%]   bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                          <a href="#">
+                          <img className="rounded-t-lg" src={info.i.imageUrl} alt={info.l} />
+                          </a>
+                          <div className="p-2 ">
+                            
+                              <h5 className="mb-2  text-md mx-auto font-bold tracking-tight text-gray-900 dark:text-white">{info.l}</h5>
+                              <h5 className="mb-2 text-md mx-auto font-bold tracking-tight text-gray-900 dark:text-white">{info?.s}</h5>
+                              <h5 className="mb-2 text-md mx-auto font-bold tracking-tight text-gray-900 dark:text-white">{info?.q}</h5>
+                            
+                          </div>
+                  </div>
+                 ))}
+            </div>
+          )
+        }
+           
     </>
+
+    // <>
+    //   <div className="w-[12%]  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    //     <a href="#">
+    //       <img className="rounded-t-lg" src="https://m.media-amazon.com/images/M/MV5BOWZmOGQyNDctYWFjMC00NWQxLTg1YWQtMzExZWQzNjgzZDBiXkEyXkFqcGdeQXVyMTk3OTQ4ODk@._V1_.jpg" alt="" />
+    //     </a>
+    //     <div className="p-2">
+    //       <a href="#">
+    //         <h5 className="mb-2 text-2xl mx-auto font-bold tracking-tight text-gray-900 dark:text-white">
+    //           The Boy 2021
+    //         </h5>
+    //       </a>
+    //       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"></p>
+    //       <a
+    //         href="#"
+    //         className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    //       >
+    //         Download
+    //         <FaDownload className="mx-2" />
+    //       </a>
+    //     </div>
+    //   </div>
+    // </>
   );
 };
+
+
+
+// function Card() {
+//   const { data, loading, error } = useFetch("spiderman");
+
+//   console.table(data);
+
+//   return (
+//     <section>
+//       {loading ? (
+//         <p>Loading...</p>
+//       ) : error ? (
+//         <h1>{error}</h1>
+//       ) : (
+//         <div className="list">
+//           {data.map((info, id) => (
+//             <div className="card" key={id}>
+//               {info.i && (
+//                 <img src={info.i.imageUrl} alt={info.l} width={"80%"} />
+//               )}
+//               <h2>{info.l}</h2>
+//               <p>{info?.s}</p>
+//               <p>{info?.q}</p>
+//             </div>
+//           ))}
+//         </div>
+//       )}
+//     </section>
+//   );
+// }
 
 export default Card;
--- src/components/Hooks/useInfoHook.js
+++ src/components/Hooks/useInfoHook.js
@@ -1,31 +1,119 @@
-"usestrict"
+// // import axios from "axios";
+// // import { useState, useEffect } from "react";
+
+// // const useInfo = function (query=spiderman) {
+// //   const [info, setInfo] = useState([]);
+
+// //   const rapidHeaders= {
+// //     "X-RapidAPI-Key": "1026cea75bmsh3d6c23b626ef8f8p1ca301jsnd209f88f37f8",
+// //     "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
+// //   }
+// //   const config={
+// //     method: "GET",
+// //     url: `https://imdb8.p.rapidapi.com/auto-complete?p=${query}`,
+// //     headers:rapidHeaders
+// //   };
+
+// //   const fetchData=async ()=>{
+// //     await axios.request(config)
+// //     .then((res)=>setInfo(res.data))
+// //     .catch((err)=>{
+// //      console.log("Err!.There was an error in fetching the data from the APi ",err)
+// //     })
+// //   }
+// //   useEffect(() => {
+// //    fetchData()
+// //   }, []);
+
+// //   console.log(info)
+// // };
+
+// // export default useInfo;
+
+// import axios from "axios";
+// import { useState, useEffect } from "react";
+
+// const useInfo = function (query) {
+//   const [info, setInfo] = useState([]);
+
+//   const rapidHeaders = {
+//     "X-RapidAPI-Key": "1026cea75bmsh3d6c23b626ef8f8p1ca301jsnd209f88f37f8",
+//     "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
+//   };
+
+//   const config = {
+//     method: "GET",
+//     url: `https://imdb8.p.rapidapi.com/auto-complete?p=${query}`,
+//     headers: rapidHeaders,
+//   };
+
+//   const fetchData = async () => {
+//     try {
+//       const { status, data } = await axios(config);
+//       console.log(data.d); 
+//       if (status === 200) {
+//         setInfo(data.d);
+//       }
+//     } catch (err) {
+//       console.log(
+//         "Error! There was an error in fetching the d from the API ",
+//         err
+//       );
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   return info;
+// };
+
+// export default useInfo;
+
+
+
 
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const useInfo = function () {
-  const [info, setInfo] = useState({});
+function useFetch(query) {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+
+  const rapidApiHeaders = {
+    "X-RapidAPI-Key": "1026cea75bmsh3d6c23b626ef8f8p1ca301jsnd209f88f37f8",
+    "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
+  };
+
+  const config = {
+    method: "GET",
+    url: `https://imdb8.p.rapidapi.com/auto-complete?q=${query}`,
+    headers: rapidApiHeaders,
+  };
+
+  const fetchData = async () => {
+    setError("");
+    setLoading(true);
+    try {
+      const { status, data } = await axios.request(config);
+      console.log(data.d)
+      if (status === 200) setData(data.d);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    axios({
-      method: "GET",
-      url: "https://imdb8.p.rapidapi.com/auto-complete",
-      params: { q: "game of thr" },
-      headers: {
-        "X-RapidAPI-Key": "1026cea75bmsh3d6c23b626ef8f8p1ca301jsnd209f88f37f8",
-        "X-RapidAPI-Host": "imdb8.p.rapidapi.com",
-      },
-    })
-      .then(res => setInfo(res)       )
-      .catch((err) => {
-        console.error("Error in fetching the data ", err);
-      });
+    if (query) {
+      fetchData();
+    }
   }, []);
 
-  return info.d.map((items) => {
-    console.log(items.q);
-  });
-};
+  return { loading, data, error };
+}
 
-export default useInfo;
-
-
+export default useFetch;
